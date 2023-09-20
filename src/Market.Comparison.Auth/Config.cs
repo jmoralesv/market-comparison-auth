b3f5585d08397718d65a804c0289a7aa8fa1cc07
jmoralesv <korum03@gmail.com>
2023-09-19 21:31:29 -0500

    Changes for adding Duende.IdentityServer and setting it up in the project, for in memory scopes and clients.

---
+++ src/Market.Comparison.Auth/Config.cs
@@ -0,0 +1,29 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace Market.Comparison.Auth;
+
+public static class Config
+{
+    private const string MarketComparisonApiScope = "Market.Comparison.Api";
+
+    public static IEnumerable<ApiScope> ApiScopes =>
+        new List<ApiScope>
+        {
+            new ApiScope(MarketComparisonApiScope, "Market Comparison API")
+        };
+
+    public static IEnumerable<Client> Clients =>
+        new List<Client>
+        {
+            new Client
+            {
+                ClientId = "client",
+                AllowedGrantTypes = GrantTypes.ClientCredentials,// no interactive user, use the clientid/secret for authentication
+                ClientSecrets =
+                {
+                    new Secret("secret".Sha256())// secret for authentication
+                },
+                AllowedScopes = { MarketComparisonApiScope }// scopes that client has access to
+            }
+        };
+}
--- src/Market.Comparison.Auth/Program.cs
+++ src/Market.Comparison.Auth/Program.cs
@@ -1,44 +1,25 @@
+using Market.Comparison.Auth;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddIdentityServer()
+    .AddInMemoryApiScopes(Config.ApiScopes)
+    .AddInMemoryClients(Config.Clients);
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+app.UseIdentityServer();
 
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
+app.UseWelcomePage(new WelcomePageOptions { Path = "/" });
 
-app.Run();
-
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+await app.RunAsync();
