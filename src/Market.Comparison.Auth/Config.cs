ac5fed1bea5c736da8daca1723e9a902ef1ac98b
jmoralesv <korum03@gmail.com>
2023-09-27 23:54:44 -0500

    Adding changes for having the Identity Server UI in this project.

--- src/Market.Comparison.Auth/Config.cs
+++ src/Market.Comparison.Auth/Config.cs
@@ -6,6 +6,13 @@ public static class Config
 {
     private const string MarketComparisonApiScope = "Market.Comparison.Api";
 
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource>
+        {
+            new IdentityResources.OpenId(),
+            new IdentityResources.Profile(),
+        };
+
     public static IEnumerable<ApiScope> ApiScopes =>
         new List<ApiScope>
         {
