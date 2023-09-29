c5feec70fa54cc83455da637c842e272a17e14da
jmoralesv <korum03@gmail.com>
2023-09-29 00:23:58 -0500

    Adding some extra changes, to follow the tutorial.

--- src/Market.Comparison.Auth/Config.cs
+++ src/Market.Comparison.Auth/Config.cs
@@ -1,4 +1,5 @@
-﻿using Duende.IdentityServer.Models;
+﻿using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
 
 namespace Market.Comparison.Auth;
 
@@ -31,6 +32,26 @@ public static class Config
                     new Secret("secret".Sha256())// secret for authentication
                 },
                 AllowedScopes = { MarketComparisonApiScope }// scopes that client has access to
+            },
+            // interactive ASP.NET Core Web App
+            new Client
+            {
+                ClientId = "web",
+                ClientSecrets = { new Secret("secret".Sha256()) },
+
+                AllowedGrantTypes = GrantTypes.Code,
+            
+                // where to redirect to after login
+                RedirectUris = { "https://localhost:7102/signin-oidc" },
+
+                // where to redirect to after logout
+                PostLogoutRedirectUris = { "https://localhost:7102/signout-callback-oidc" },
+
+                AllowedScopes = new List<string>
+                {
+                    IdentityServerConstants.StandardScopes.OpenId,
+                    IdentityServerConstants.StandardScopes.Profile
+                }
             }
         };
 }
--- src/Market.Comparison.Auth/TestUsers.cs
+++ src/Market.Comparison.Auth/TestUsers.cs
@@ -1,11 +1,11 @@
 // Copyright (c) Duende Software. All rights reserved.
 // See LICENSE in the project root for license information.
 
-using IdentityModel;
 using System.Security.Claims;
 using System.Text.Json;
 using Duende.IdentityServer;
 using Duende.IdentityServer.Test;
+using IdentityModel;
 
 namespace Market.Comparison.Auth;
 
