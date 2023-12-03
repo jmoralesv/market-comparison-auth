641ab5b28119ca6beb8991bdc84fe4a32863c3f6
jmoralesv <korum03@gmail.com>
2023-12-03 00:32:22 -0500

    Changes on how the data is seeded and a new migration is added.

--- src/Market.Comparison.Auth.Data/Contexts/CustomConfigurationDbContext.cs
+++ src/Market.Comparison.Auth.Data/Contexts/CustomConfigurationDbContext.cs
@@ -4,10 +4,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Market.Comparison.Auth.Data.Contexts;
 
-public class CustomConfigurationDbContext(DbContextOptions<ConfigurationDbContext> options) : ConfigurationDbContext(options)
+public class CustomConfigurationDbContext(DbContextOptions<CustomConfigurationDbContext> options)
+    : ConfigurationDbContext<CustomConfigurationDbContext>(options)
 {
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //Enable this to debug when adding a new migration
+        //This is for the Seed method below
+        //System.Diagnostics.Debugger.Launch();
+
         ArgumentNullException.ThrowIfNull(modelBuilder);
 
         base.OnModelCreating(modelBuilder);
--- src/Market.Comparison.Auth.Data/Extensions/DataLoadingExtensions.cs
+++ src/Market.Comparison.Auth.Data/Extensions/DataLoadingExtensions.cs
@@ -9,12 +9,132 @@ internal static class DataLoadingExtensions
 {
     internal static void Seed(this ModelBuilder modelBuilder)
     {
-        var clients = Config.Clients.Select(x => x.ToEntity());
-        var resources = Config.IdentityResources.Select(x => x.ToEntity());
-        var scopes = Config.ApiScopes.Select(x => x.ToEntity());
+        var clientGrantTypes = new List<ClientGrantType>();
+        var clientSecrets = new List<ClientSecret>();
+        var clientScopes = new List<ClientScope>();
+        var clientRedirectUris = new List<ClientRedirectUri>();
+        var clientPostLogoutRedirectUris = new List<ClientPostLogoutRedirectUri>();
+        var identityResourceClaims = new List<IdentityResourceClaim>();
+
+        var clients = Config.Clients
+            .Select(x => x.ToEntity())
+            .Select((item, i) =>
+            {
+                item.Id = i + 1;
+
+                if (item.AllowedGrantTypes.Count != 0)
+                {
+                    item.AllowedGrantTypes = item.AllowedGrantTypes
+                        .Select(agt => { agt.ClientId = item.Id; return agt; })
+                        .ToList();
+
+                    clientGrantTypes.AddRange(item.AllowedGrantTypes);
+                }
+
+                if (item.ClientSecrets.Count != 0)
+                {
+                    item.ClientSecrets = item.ClientSecrets
+                        .Select(cs => { cs.ClientId = item.Id; return cs; })
+                        .ToList();
+
+                    clientSecrets.AddRange(item.ClientSecrets);
+                }
+
+                if (item.AllowedScopes.Count != 0)
+                {
+                    item.AllowedScopes = item.AllowedScopes
+                        .Select(s => { s.ClientId = item.Id; return s; })
+                        .ToList();
+
+                    clientScopes.AddRange(item.AllowedScopes);
+                }
+
+                if (item.RedirectUris.Count != 0)
+                {
+                    item.RedirectUris = item.RedirectUris
+                        .Select(ru => { ru.ClientId = item.Id; return ru; })
+                        .ToList();
+
+                    clientRedirectUris.AddRange(item.RedirectUris);
+                }
+
+                if (item.PostLogoutRedirectUris.Count != 0)
+                {
+                    item.PostLogoutRedirectUris = item.PostLogoutRedirectUris
+                        .Select(plru => { plru.ClientId = item.Id; return plru; })
+                        .ToList();
+
+                    clientPostLogoutRedirectUris.AddRange(item.PostLogoutRedirectUris);
+                }
+
+                item.AllowedGrantTypes = null;
+                item.ClientSecrets = null;
+                item.AllowedScopes = null;
+                item.RedirectUris = null;
+                item.PostLogoutRedirectUris = null;
+
+                return item;
+            })
+            .ToList();
+
+        clientGrantTypes = clientGrantTypes
+            .Select((item, i) => { item.Id = i + 1; return item; })
+            .ToList();
+
+        clientSecrets = clientSecrets
+            .Select((item, i) => { item.Id = i + 1; return item; })
+            .ToList();
+
+        clientScopes = clientScopes
+            .Select((item, i) => { item.Id = i + 1; return item; })
+            .ToList();
+
+        clientRedirectUris = clientRedirectUris
+            .Select((item, i) => { item.Id = i + 1; return item; })
+            .ToList();
+
+        clientPostLogoutRedirectUris = clientPostLogoutRedirectUris
+            .Select((item, i) => { item.Id = i + 1; return item; })
+            .ToList();
+
+        var resources = Config.IdentityResources
+            .Select(x => x.ToEntity())
+            .Select((item, i) =>
+            {
+                item.Id = i + 1;
+
+                if (item.UserClaims.Count != 0)
+                {
+                    item.UserClaims = item.UserClaims
+                        .Select(uc => { uc.IdentityResourceId = item.Id; return uc; })
+                        .ToList();
+
+                    identityResourceClaims.AddRange(item.UserClaims);
+                }
+
+                item.UserClaims = null;
+
+                return item;
+            })
+            .ToList();
+
+        identityResourceClaims = identityResourceClaims
+            .Select((item, i) => { item.Id = i + 1; return item; })
+            .ToList();
+
+        var scopes = Config.ApiScopes
+            .Select(x => x.ToEntity())
+            .Select((item, i) => { item.Id = i + 1; return item; })
+            .ToList();
 
         modelBuilder.Entity<Client>().HasData(clients);
+        modelBuilder.Entity<ClientGrantType>().HasData(clientGrantTypes);
+        modelBuilder.Entity<ClientSecret>().HasData(clientSecrets);
+        modelBuilder.Entity<ClientScope>().HasData(clientScopes);
+        modelBuilder.Entity<ClientRedirectUri>().HasData(clientRedirectUris);
+        modelBuilder.Entity<ClientPostLogoutRedirectUri>().HasData(clientPostLogoutRedirectUris);
         modelBuilder.Entity<IdentityResource>().HasData(resources);
+        modelBuilder.Entity<IdentityResourceClaim>().HasData(identityResourceClaims);
         modelBuilder.Entity<ApiScope>().HasData(scopes);
     }
 }
